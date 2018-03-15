e48b7ca54db6adf9cf113032071811bd606898ac
Jacek Kopecky <jacek@jacek.cz>
2018-03-15 11:11:54 +0000

    ready for testing in the cloud

--- web-serve-machine-name/server.js
+++ web-serve-machine-name/server.js
@@ -4,11 +4,34 @@ const express = require('express');
 const fetch = require('node-fetch');
 
 async function init() {
-  let response = await fetch('http://metadata.google.internal/computeMetadata/v1/instance/name', { headers: { 'Metadata-Flavor': 'Google' } });
+  let response = await fetch(
+    'http://metadata.google.internal/computeMetadata/v1/instance/name',
+    { headers: { 'Metadata-Flavor': 'Google' } },
+  );
   if (!response.ok) {
     console.log('failed getting instance name', response);
     return;
   }
 
   const instName = await response.text();
+
+  response = await fetch(
+    'http://metadata.google.internal/computeMetadata/v1/instance/zone',
+    { headers: { 'Metadata-Flavor': 'Google' } },
+  );
+  if (!response.ok) {
+    console.log('failed getting instance zone', response);
+    return;
+  }
+
+  const instZone = await response.text();
+
+  const app = express();
+  app.get('/', (req, res) => res.send(`hello from instance ${instName} in zone ${instZone}`));
+
+  await app.listen(process.env.PORT || 8080);
+
+  console.log('started');
 }
+
+init().then(null, console.error);
